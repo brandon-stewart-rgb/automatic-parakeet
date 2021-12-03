f810923c214a263a264cf981052051be9ef1a4d5
brandon-stewart-rgb <brandon-stewart@pixeltree.us>
2021-12-03 12:14:52 -0700

    registeration page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -6,12 +6,14 @@ export default function Register() {
            
           <form className="registerForm">
           <span className="registerTitle">Register</span>
+              <label>Username</label>
+              <input type="text" className='registerInput' placeholder='Enter your username...' />
               <label>Email</label>
               <input type="text" className='registerInput' placeholder='Enter your email...' />
               <label>Password</label>
               <input type="password" className='registerInput' placeholder='Enter your password...'  />
               <button className="registerButton">Register</button>
-              <button className="registerRegisterButton">Login</button>
+              <button className="registerLoginButton">Login</button>
               
           </form>
         
