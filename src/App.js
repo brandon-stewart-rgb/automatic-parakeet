a2aa6a37ac755789af319b48d95f9fc9f0b31f52
brandon-stewart-rgb <brandon-stewart@pixeltree.us>
2021-12-04 14:48:11 -0700

    routes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,38 @@ import Write from './pages/write/Write';
 import Settings from './pages/settings/Settings';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link
+  } from "react-router-dom";
+  
 
 function App() {
+	const user = false;
 	return (
-		<>
+		<Router>
 			<TopBar />
-			<Register />
-		</>
+			<Routes>
+
+			<Route exact path="/" element={<Home/>} />
+
+			<Route path="/register" element={<Register/>} />
+
+			<Route path="/login" element={<Login/>} />
+
+			<Route path="/write" element={<Write/>} />
+
+			<Route path="/settings" element={<Settings/>} />
+
+			<Route path="/post/:postId" element={<Single />} />
+		
+
+			</Routes>
+			
+		</Router>
 	);
 }
 
