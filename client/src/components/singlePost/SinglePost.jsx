92e13cd76092894e07ed6bb81114ccb4ca879b6b
brandon-stewart-rgb <brandon-stewart@pixeltree.us>
2021-12-18 12:49:33 -0700

    Single Post, Cats, Register

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,30 +1,46 @@
+import axios from 'axios';
+import { useLocation } from 'react-router';
+import { useEffect, useState } from 'react';
 import './singlePost.css';
+import { Link } from 'react-router-dom';
 
 export default function SinglePost() {
-    return (
-        <div className='singlePost'>
-          <div className="singlePostWrapper">
-              <img src="https://images.pexels.com/photos/10008380/pexels-photo-10008380.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" className="singlePostImg" />
-              <h1 className="singlePostTitle">Lorem ipsum dolor nemo!
-              <div className="singlePostEdit">
-                  <i className="singlePostIcon far fa-edit"></i>
-                  <i className="singlePostIcon far fa-trash-alt"></i>
-                  </div>
-              </h1>
-              <div className="singlePostInfo">
-                  <span className='SinglePostAuthor'>Author: <b>Brandon</b></span>
-                  <span className='SinglePostDate'>1 hour</span>
-              </div>
-              <p className='singlePostDesc'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis in quo laudantium, inventore ducimsequs magni facere molestias labore nam velit.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis in quo laudantium, inventore ducimsequs magni facere molestias labore nam velit.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis in quo laudantium, inventore ducimsequs magni facere molestias labore nam velit.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis in quo laudantium, inventore ducimsequs magni facere molestias labore nam velit.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis in quo laudantium, inventore ducimsequs magni facere molestias labore nam velit.
-                  
-              </p>
-              
-          </div>
-        </div>
-    )
+	const location = useLocation();
+	// splitting url and taking second item which is post id... 0,1,2
+	const path = location.pathname.split('/')[2];
+	const [post, setPost] = useState({});
+	// whenever path changes, fire useEffect
+	useEffect(() => {
+		const getPost = async () => {
+			const res = await axios.get('/posts/' + path);
+			setPost(res.data);
+		};
+		getPost();
+	}, [path]);
+	return (
+		<div className="singlePost">
+			<div className="singlePostWrapper">
+				{post.photo && (
+					<img src={post.photo} alt="" className="singlePostImg" />
+				)}
+				<h1 className="singlePostTitle">
+					{post.title}
+					<div className="singlePostEdit">
+						<i className="singlePostIcon far fa-edit"></i>
+						<i className="singlePostIcon far fa-trash-alt"></i>
+					</div>
+				</h1>
+				<div className="singlePostInfo">
+					<span className="SinglePostAuthor">
+						Author:
+                        <Link to={`/?user=${post.username}`} className='link'> <b>{post.username}</b></Link>
+					</span>
+					<span className="SinglePostDate">
+						{new Date(post.createdAt).toDateString()}
+					</span>
+				</div>
+				<p className="singlePostDesc"> {post.desc}</p>
+			</div>
+		</div>
+	);
 }
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,25 +1,64 @@
-import { Link } from 'react-router-dom'
-import './register.css'
+import axios from 'axios';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './register.css';
 
 export default function Register() {
-    return (
-        <div className='register'>
-           
-          <form className="registerForm">
-          <span className="registerTitle">Register</span>
-              <label>Username</label>
-              <input type="text" className='registerInput' placeholder='Enter your username...' />
-              <label>Email</label>
-              <input type="text" className='registerInput' placeholder='Enter your email...' />
-              <label>Password</label>
-              <input type="password" className='registerInput' placeholder='Enter your password...'  />
-              <button className="registerButton">Register</button>
-              <button className="registerLoginButton">
-                  <Link to='/login' className='link'>Login</Link>
-              </button>
-              
-          </form>
-        
-        </div>
-    )
+	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState(false);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+        setError(false);
+        try{
+            const res = await axios.post('/auth/register', {
+                username,
+                email,
+                password,
+            });  
+            //if successful registration it will redirect to login page
+            res.data && window.location.replace('/login')
+        }catch(err){
+            setError(true);
+        }
+	};
+	return (
+		<div className="register">
+			<form className="registerForm" onSubmit={handleSubmit}>
+				<span className="registerTitle">Register</span>
+				<label>Username</label>
+				<input
+					type="text"
+					className="registerInput"
+					placeholder="Enter your username..."
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<label>Email</label>
+				<input
+					type="text"
+					className="registerInput"
+					placeholder="Enter your email..."
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<label>Password</label>
+				<input
+					type="password"
+					className="registerInput"
+					placeholder="Enter your password..."
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button className="registerButton" type="submit">
+					Register
+				</button>
+				<button className="registerLoginButton">
+					<Link to="/login" className="link">
+						Login
+					</Link>
+				</button>
+        {error && <span>Something went wrong!</span>}
+			</form>
+		</div>
+	);
 }
